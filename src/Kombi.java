1394078dd11e63ffe6566b552bc0086074da73d3
gokayari <gokayari@gmail.com>
2024-03-13 19:49:01 +0100

    Unterklassen

---
+++ src/Kombi.java
@@ -0,0 +1,7 @@
+public class Kombi extends Auto{
+
+    public Kombi(String erstzulassung, int kmZustand,
+                 double preis, int anzahlDerTüren){
+        super("Kombi", erstzulassung, kmZustand, preis, anzahlDerTüren);
+    }
+}
---
+++ src/Limousine.java
@@ -0,0 +1,7 @@
+public class Limousine extends Auto{
+
+    public Limousine( String erstzulassung, int kmZustand,
+                     double preis, int anzahlDerTüren){
+        super("Limousine", erstzulassung, kmZustand, preis, anzahlDerTüren);
+    }
+}
