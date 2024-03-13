6cc4d4b8d1e618fce1d9c6e6ff4ffead4eeaaa16
gokayari <gokayari@gmail.com>
2024-03-13 22:44:03 +0100

    Oberklasse aktualisiert

--- src/Auto.java
+++ src/Auto.java
@@ -1,19 +1,44 @@
 public class Auto {
 
+    private String marke;
     private String bauform;
     private String erstzulassung;
     private int kmZustand;
     private double preis;
     private int anzahlDerTüren;
 
-    public Auto(String bauform,String erstzulassung, int kmZustand,
+    public Auto(String marke, String bauform,String erstzulassung, int kmZustand,
                 double preis, int anzahlDerTüren){
+        this.marke = marke;
         this.bauform = bauform;
         this.erstzulassung = erstzulassung;
         this.kmZustand = kmZustand;
         this.preis = preis;
         this.anzahlDerTüren = anzahlDerTüren;
+    }
 
+    public String getMarke() {
+        return marke;
+    }
+
+    public String getBauform() {
+        return bauform;
+    }
+
+    public String getErstzulassung() {
+        return erstzulassung;
+    }
+
+    public int getKmZustand() {
+        return kmZustand;
+    }
+
+    public double getPreis() {
+        return preis;
+    }
+
+    public int getAnzahlDerTüren() {
+        return anzahlDerTüren;
     }
 
     public void autoFahren(int gefahrenerKm){
